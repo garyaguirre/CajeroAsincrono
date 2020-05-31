f3f91aba4056b026d90634815c0e0ee09b0f7490
garyaguirre <gary.aguirre@upeu.edu.pe>
2020-05-31 00:17:50 -0500

    Cajero

---
+++ src/cajero/Usuario.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cajero;
+
+/**
+ *
+ * @author CASA
+ */
+public class Usuario extends thread{
+    public void run(){
+        System.out.println("IniciaCajero");
+    }
+
+    void start() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
